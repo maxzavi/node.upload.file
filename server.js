a2583a450e675e59793cb139ea994e8cd81278f0
Max Zavaleta <mzavaletav@gmail.com>
2022-09-25 00:48:53 -0500

    feat:  add rename after upload

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ const multer = require("multer");
 
 const app = express();
 
+const pathTarget='/files';
 const fileStorageEngine = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "./files"); //important this is a direct path from our current file to storage location
-    },
-    filename: (req, file, cb) => {
-      cb(null, "Upload_"+ file.originalname);
-    },
-  });
+  destination: (req, file, cb) => {
+    cb(null, pathTarget); //important this is a direct path from our current file to storage location
+  },
+  filename: (req, file, cb) => {
+    cb(null, "Upload_"+ file.originalname);
+  },
+});
 
 const upload = multer({ storage: fileStorageEngine });
  
 //'file' name in field file
 app.post('/upload', upload.single('filename') , (req, res)=>{
-    res.send(`File ${req.file.filename} upload OK!!`);
+  //Rename after post file
+  fs.renameSync(`${pathTarget}/Upload_${req.file.originalname}`, 
+    `${pathTarget}/${req.file.originalname}`);
+  res.send(`File ${req.file.filename} upload OK!!`);
 });
   
 app.listen(3000,()=>{
